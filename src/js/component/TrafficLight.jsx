0b4772516a73c38fa6138fcf7205ed25aad7f2a7
Shafik Bahou <snb3000@gmail.com>
2024-07-02 00:52:10 +0000

    interface updates

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,18 +1,57 @@
 import React from "react";
 
 export const TrafficLight = (props) => {
+  const [redLightOn, setRedLightOn] = React.useState(false);
+  const [yellowLightOn, setYellowLightOn] = React.useState(false);
+  const [greenLightOn, setGreenLightOn] = React.useState(false);
+  const [firstLightColor, setFirstLightColor] = React.useState("undefined");
   return (
-    <div className="traffic-light-box bg-dark d-flex flex column justify-content-between">
-      <div
-        className={
-          redLightOn === true ? "light bg-danger" : "light bg-secondary"
-        }
-        onClick={(event) => {
-          setRedLightOn(!redLightOn);
-        }}
-      ></div>
-      <div className="light bg-secondary"></div>
-      <div className="light bg-secondary"></div>
-    </div>
+    <React.Fragment>
+      <div className="traffic-light-box bg-dark d-flex flex-column justify-content-between">
+        <div
+          className={
+            redLightOn === true ? "light bg-danger" : "light bg-secondary"
+          }
+          onClick={(event) => {
+            setRedLightOn(!redLightOn);
+          }}
+        ></div>
+        <div
+          className={
+            yellowLightOn === true ? "light bg-warning" : "light bg-secondary"
+          }
+          onClick={(event) => {
+            setYellowLightOn(!yellowLightOn);
+          }}
+        ></div>
+        <div
+          className={
+            greenLightOn === true ? "light bg-success" : "light bg-secondary"
+          }
+          onClick={(event) => {
+            setGreenLightOn(!greenLightOn);
+          }}
+        ></div>
+      </div>
+      <div className="d-flex flex-column justify-space-around">
+        <div className="d-flex-justify-space-between">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="some color"
+            value={firstLightColor}
+            onKeyDown={(event) => {
+              if (event.key === ";") {
+                console.log(event.key);
+              }
+              event.stopPropagation();
+            }}
+            onChange={(event) => {
+              setFirstLightColor(event.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,8 +7,10 @@ import { TrafficLight } from "./TrafficLight";
 //create your first component
 const Home = () => {
   return (
-    <div className="container w-100 d-flex">
-      <TrafficLight />
+    <div className="container w-100 d-flex justify-content-center align-items-center">
+      <div className="d-flex flex-column align-items-center">
+        <TrafficLight />
+      </div>
     </div>
   );
 };
