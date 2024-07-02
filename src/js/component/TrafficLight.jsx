082616ef6e229fe16b1d3f9d09be48802c62e699
Shafik Bahou <snb3000@gmail.com>
2024-07-02 00:24:29 +0000

    traffic light interactivity updates

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,7 +3,14 @@ import React from "react";
 export const TrafficLight = (props) => {
   return (
     <div className="traffic-light-box bg-dark d-flex flex column justify-content-between">
-      <div className="light bg-secondary"></div>
+      <div
+        className={
+          redLightOn === true ? "light bg-danger" : "light bg-secondary"
+        }
+        onClick={(event) => {
+          setRedLightOn(!redLightOn);
+        }}
+      ></div>
       <div className="light bg-secondary"></div>
       <div className="light bg-secondary"></div>
     </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,18 +7,8 @@ import { TrafficLight } from "./TrafficLight";
 //create your first component
 const Home = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Hello Rigo!</h1>
-      <p>
-        <img src={rigoImage} />
-      </p>
-      <a href="#" className="btn btn-success">
-        If you see this green button... bootstrap is working...
-      </a>
-      <p>
-        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-        love!
-      </p>
+    <div className="container w-100 d-flex">
+      <TrafficLight />
     </div>
   );
 };
