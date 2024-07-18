ed047d14d0b7fc111476f6b56ce9159541286aa2
Shafik Bahou <snb3000@gmail.com>
2024-07-18 03:20:17 +0000

    lights turn on upon clicking

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,57 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TrafficLight = (props) => {
-  const [redLightOn, setRedLightOn] = React.useState(false);
-  const [yellowLightOn, setYellowLightOn] = React.useState(false);
-  const [greenLightOn, setGreenLightOn] = React.useState(false);
-  const [firstLightColor, setFirstLightColor] = React.useState("undefined");
+export const TrafficLight = () => {
+  const [activeLight, setActiveLight] = useState("red");
+
   return (
-    <React.Fragment>
-      <div className="traffic-light-box bg-dark d-flex flex-column justify-content-between">
-        <div
-          className={
-            redLightOn === true ? "light bg-danger" : "light bg-secondary"
-          }
-          onClick={(event) => {
-            setRedLightOn(!redLightOn);
-          }}
-        ></div>
-        <div
-          className={
-            yellowLightOn === true ? "light bg-warning" : "light bg-secondary"
-          }
-          onClick={(event) => {
-            setYellowLightOn(!yellowLightOn);
-          }}
-        ></div>
-        <div
-          className={
-            greenLightOn === true ? "light bg-success" : "light bg-secondary"
-          }
-          onClick={(event) => {
-            setGreenLightOn(!greenLightOn);
-          }}
-        ></div>
-      </div>
-      <div className="d-flex flex-column justify-space-around">
-        <div className="d-flex-justify-space-between">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="some color"
-            value={firstLightColor}
-            onKeyDown={(event) => {
-              if (event.key === ";") {
-                console.log(event.key);
-              }
-              event.stopPropagation();
-            }}
-            onChange={(event) => {
-              setFirstLightColor(event.target.value);
-            }}
-          />
-        </div>
-      </div>
-    </React.Fragment>
+    <div className="traffic-light-box bg-dark d-flex flex-column justify-content-between">
+      <div
+        className={
+          activeLight === "red" ? "light bg-danger" : "light bg-secondary"
+        }
+        onClick={() => setActiveLight("red")}
+      ></div>
+      <div
+        className={
+          activeLight === "yellow" ? "light bg-warning" : "light bg-secondary"
+        }
+        onClick={() => setActiveLight("yellow")}
+      ></div>
+      <div
+        className={
+          activeLight === "green" ? "light bg-success" : "light bg-secondary"
+        }
+        onClick={() => setActiveLight("green")}
+      ></div>
+    </div>
   );
 };
